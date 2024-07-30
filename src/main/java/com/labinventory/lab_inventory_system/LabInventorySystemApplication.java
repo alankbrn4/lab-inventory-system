4e4fad860e1d385b2060f5e434180d67c313c41e
ALAN47 <alankbrn4@gmail.com>
2024-07-30 16:28:14 -0600

    Creación de archivo war para tomcat

--- src/main/java/com/labinventory/lab_inventory_system/LabInventorySystemApplication.java
+++ src/main/java/com/labinventory/lab_inventory_system/LabInventorySystemApplication.java
@@ -2,9 +2,17 @@ package com.labinventory.lab_inventory_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class LabInventorySystemApplication {
+public class LabInventorySystemApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LabInventorySystemApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LabInventorySystemApplication.class, args);
